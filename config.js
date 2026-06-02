47b51c7d6d62148049cb78c1560c207362981ecf
AwkwardMan143 <mkrans5522@gmail.com>
2026-06-02 17:45:44 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Edit this after you expose your bot API to the internet (ngrok, Cloudflare Tunnel, VPS, etc.)
 // Example: "https://abc123.ngrok-free.app"
 // Do NOT include a trailing slash.
-const BOT_API_URL = "https://buffed-bonsai-stool.ngrok-free.dev";
-window.BOT_API_URL = http://127.0.0.1:5000;
+const BOT_API_URL = "http://127.0.0.1:5000";
+window.BOT_API_URL = https://buffed-bonsai-stool.ngrok-free.dev;
