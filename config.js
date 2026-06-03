d94cb434277d698b6b895cdeb2872d84fba2193e
AwkwardMan143 <mkrans5522@gmail.com>
2026-06-03 09:55:47 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const BOT_API_URL = "https://wav-jumping-bald-derby.trycloudflare.com";
+const BOT_API_URL = "https://driving-numeric-arise-vision.trycloudflare.com";
 window.BOT_API_URL = BOT_API_URL;
