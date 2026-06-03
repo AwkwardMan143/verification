c7425a6928a47eef0b6aae1798dd79b108980593
AwkwardMan143 <mkrans5522@gmail.com>
2026-06-03 10:00:28 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const BOT_API_URL = "https://driving-numeric-arise-vision.trycloudflare.com";
+const BOT_API_URL = "https://motorola-planets-blonde-eric.trycloudflare.com";
 window.BOT_API_URL = BOT_API_URL;
