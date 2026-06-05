2efd10f545888b667e3404b9f7c8731e1eba7784
AwkwardMan143 <mkrans5522@gmail.com>
2026-06-04 23:57:09 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const BOT_API_URL = "https://prepared-ftp-hook-affiliated.trycloudflare.com";
+const BOT_API_URL = "https://medical-vanilla-seller-gathering.trycloudflare.com";
 window.BOT_API_URL = BOT_API_URL;
