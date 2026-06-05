97a82490af9ce06286203790684a13910db3406a
AwkwardMan143 <mkrans5522@gmail.com>
2026-06-04 20:35:19 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const BOT_API_URL = "https://motorola-planets-blonde-eric.trycloudflare.com";
+const BOT_API_URL = "https://prepared-ftp-hook-affiliated.trycloudflare.com";
 window.BOT_API_URL = BOT_API_URL;
